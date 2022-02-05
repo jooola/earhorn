751cd6196b4c992f74d58b4e53e92458227e2c52
jo <ljonas@riseup.net>
2022-02-05 11:42:44 +0100

    refactor: merge cli and earhorn into main

--- earhorn/main.py
+++ earhorn/main.py
@@ -5,16 +5,55 @@ from threading import Event as ThreadEvent
 from threading import Thread
 from typing import List, Optional
 
+import click
 from loguru import logger
 
-from .archive import Archiver
+from .archive import TIMESTAMP_FORMAT, Archiver
 from .check import check_stream
 from .event import Event, Handler
 from .silence import SilenceListener
 
 
 # pylint: disable=too-many-arguments
-def run(
+@click.command(context_settings={"max_content_width": 120})
+@click.option(
+    "--hook",
+    envvar="HOOK",
+    help="Hook to run to handle events.",
+    type=click.Path(),
+)
+@click.option(
+    "--archive-path",
+    envvar="ARCHIVE_PATH",
+    help="Path to the archive directory.",
+    type=click.Path(),
+)
+@click.option(
+    "--archive-segment-size",
+    envvar="ARCHIVE_SEGMENT_SIZE",
+    help="Archive segment size in seconds.",
+    default=3600,
+    show_default=True,
+)
+@click.option(
+    "--archive-segment-filename",
+    envvar="ARCHIVE_SEGMENT_FILENAME",
+    help="Archive segment filename (without extension).",
+    default=f"archive-{TIMESTAMP_FORMAT}",
+    show_default=True,
+)
+@click.option(
+    "--archive-segment-format",
+    envvar="ARCHIVE_SEGMENT_FORMAT",
+    help="Archive segment format.",
+    default="ogg",
+    show_default=True,
+)
+@click.argument(
+    "url",
+    envvar="URL",
+)
+def cli(
     url: str,
     hook: Optional[str],
     archive_path: Optional[str],
@@ -22,6 +61,9 @@ def run(
     archive_segment_filename: str,
     archive_segment_format: str,
 ):
+    """
+    URL of the stream.
+    """
     stop_event = ThreadEvent()
 
     def stop_handler(_signum, _frame):
