b197cb852b7bd7dfe46e1ae0bcd412950aaa506e
jo <ljonas@riseup.net>
2022-02-05 15:57:04 +0100

    feat: rename prometheus metrics names

--- earhorn/prometheus.py
+++ earhorn/prometheus.py
@@ -1,12 +1,12 @@
 from prometheus_client import Enum
 
 stream_status = Enum(
-    "stream_status",
+    "earhorn_stream_status",
     "Whether the stream is up",
     states=["up", "down"],
 )
 stream_silence = Enum(
-    "stream_silence",
+    "earhorn_stream_silence",
     "Whether the stream is silent",
     states=["up", "down"],
 )
