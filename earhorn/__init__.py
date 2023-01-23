59b386b099db99962b239649106751af05c402e2
jo <ljonas@riseup.net>
2023-01-23 19:47:08 +0100

    fix: python3.7 importlib.metatadata import

--- earhorn/__init__.py
+++ earhorn/__init__.py
@@ -1,4 +1,9 @@
-from importlib.metadata import version as get_version
+import sys
+
+if sys.version_info < (3, 8):
+    from importlib_metadata import version as get_version
+else:
+    from importlib.metadata import version as get_version
 
 version = get_version("earhorn")
 __version__ = version
---
+++ tests/main_test.py
@@ -0,0 +1,5 @@
+def test_version():
+    # pylint: disable=import-outside-toplevel
+    from earhorn import version
+
+    assert version is not None
